1760270072783e05bdd4403470fb03c58d31ac0f
LojanoE <estanislaolojano@gmail.com>
2025-12-30 11:37:53 -0500

    main

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cam-test-v12.1-offline-fix'; // Incrementamos versión
+const CACHE_NAME = 'cam-test-v12.2-offline-fix'; // Incrementamos versión
 const ASSETS = [
   './',
   './index.html',

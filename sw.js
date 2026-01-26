bc5068e5448f16d6f810802812149af81ad9d6c4
Anahi0504 <anahielizabeth_ordonez@hotmail.com>
2026-01-26 14:08:39 -0500

    Main
    Actualizacion coronamiento, orden de información y eliminar ciertos drenes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cam-test-v13.05-blob-storage'; // Version bump for Blob storage fix
+const CACHE_NAME = 'cam-test-v13.06-blob-storage'; // Version bump for Blob storage fix
 const ASSETS = [
   './',
   './index.html',

504bf6ac4a298edcab151d83d4452ff05d0b7e57
LojanoE <estanislaolojano@gmail.com>
2026-02-02 18:13:42 -0500

    MAIN

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cam-test-v13.07-blob-storage'; // Version bump for Blob storage fix
+const CACHE_NAME = 'cam-test-v13.08-blob-storage'; // Version bump for Blob storage fix
 const ASSETS = [
   './',
   './index.html',
